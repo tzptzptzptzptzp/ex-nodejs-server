83a21c388fa8c73df502badb7d6f9ee13675d9b9
tzptzptzptzptzp <tzp2539@gmail.com>
2023-07-29 00:25:17 +0900

    ✨ ルートパス以外の時にNotFoundを表示する機能を追加

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const PORT = 8000;
 
 // HTTPサーバーの作成
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-  // 返却するコンテンツを設定
-  res.write("<h1>Hello World</h1>");
-  // レスポンスの送信を完了
-  res.end();
+  if (req.url === "/") {
+    // リクエストがルートパスの場合
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    // 返却するコンテンツを設定
+    res.write("<h1>Hello World</h1>");
+    // レスポンスの送信を完了
+    res.end();
+  } else {
+    // リクエストがルートパス以外の場合
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    // 返却するコンテンツを設定
+    res.write("<h1>404 Not Found</h1>");
+    // レスポンスの送信を完了
+    res.end();
+  }
 });
 
 // サーバーを起動
